126ed86dc4b9b5193d3f466e8b901c865aacdd17
welend <74567370+welend@users.noreply.github.com>
2020-11-16 17:58:06 -0300

    Add files via upload

---
+++ precache-manifest.54d3550dc083baa1e2ed5bb26bca76ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fae4f123a500606b68c4478d10b889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f97acde3027727b1127c",
+    "url": "/static/css/main.a1a2d9eb.chunk.css"
+  },
+  {
+    "revision": "06ef0a4da8ee5ec47fcf",
+    "url": "/static/js/2.9b2fadb1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9b2fadb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97acde3027727b1127c",
+    "url": "/static/js/main.5b445d3a.chunk.js"
+  },
+  {
+    "revision": "e281087f1370faf6d9c1",
+    "url": "/static/js/runtime-main.2231b398.js"
+  }
+]);
